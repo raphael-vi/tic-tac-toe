d2dc86b0a75c65abbe9732265700fe17d2f400ca
Raphael Menezes <101662377+raphael-vi@users.noreply.github.com>
2023-05-20 22:00:22 -0300

    Completo
    A user interface está completa.
    A lógica do jogo está implementada.
    
    Talvez ainda seja necessário alguma limpeza
    
    Proximas implementações: I.A

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-const form = document.querySelector('[data-attribute="formulary"]');
+const getUserInfo = document.querySelector("[data-attribute='formulary']"); 
+const userButton = document.querySelector('[data-attribute="button"]');
+const roundDisplay = document.querySelector('[data-attribute="round-display"]');
+const turnDisplay = document.querySelector('.players-display')
+
+const userInput = () => {
+    const playerOne = document.querySelector("[data-attribute='playerOne']").value
+    const playerTwo = document.querySelector("[data-attribute='playerTwo']").value 
+
+    const pOneDisplay = document.querySelector('[data-attribute="pOneDisplay"]')
+    const pTwoDisplay = document.querySelector('[data-attribute="pTwoDisplay"]')
+    pOneDisplay.textContent =` ${playerOne}: X`;
+    pTwoDisplay.textContent = ` ${playerTwo}: O`
+
+    return createPlayers(playerOne, playerTwo);
+};
+
+
+
+getUserInfo.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    gameBoard.clearBoard()
+    const players = userInput()
+    gameController.setPlayers(players);
+    roundDisplay.textContent= ''
+    getUserInfo.reset();
+
+   
+      
+});
+
 
 
 
 const createPlayers = (playerOneName ='Player One', playerTwoName = 'Player Two') =>{   
 
-    const players = [
-        playerOne={
-            name: playerOneName,
-            token: 'X'
-        },
-        playerTwo={
-            name: playerTwoName,
-            token: 'O'
-        }
-    ]
-    return players   
+    const playerOne={
+        name: playerOneName || 'Player One',
+        token: 'X'
+    }
+
+    const playerTwo={
+        name: playerTwoName || 'Player Two',
+        token: 'O'
+    }
+    
+    const players = [playerOne, playerTwo];
+    
+    return players;  
 }
 
 const gameBoard = (()=>{
+    
     const squares = document.querySelectorAll(".square");
+    
+    
+
     
     const row = []
     const column = []
@@ -46,99 +83,148 @@ const gameBoard = (()=>{
     const markSquare = (square, playerToken)=>{
         square.textContent = playerToken
     }
-    
-    const resetBoard = () =>{
-        squares.forEach(square => {
-            square.textContent = ''
-        });
-    };  
-   
+
+    const clearBoard = () =>{
+        squares.forEach(square => square.textContent = '');
+        turnDisplay.textContent = '';
+        gameController.clearGame();
+    }
 
     return{
         squares,
         markSquare,
-        resetBoard,
         row,
         column,
-        diagonal
+        diagonal,
+        clearBoard
     };
 })()
 
 
 
 const gameController = (()=>{
-   
-   
-   
-   
-    let activePlayer = players[0]
+    let players = createPlayers();
+    let activePlayer = players[0];
+    
+    const setPlayers = (newPlayers) => {
+        players = newPlayers;
+        console.log(players);
+        activePlayer = players[0];
+    };
+     
     let gameOver = false;
     
-
     const switchPlayers = () =>{             
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        
     };
     
-
-
     const playRound = (square) =>{
-        console.log(players)
+        console.log(gameOver)
         const currentPlayer = activePlayer;
         const winCondition = currentPlayer.token.repeat(3);
         const boardRow = gameBoard.row;
         const boardColumn = gameBoard.column;
         const boardDiagonal = gameBoard.diagonal;
-
-        if (gameOver === true) return;
-        if (square.textContent !== '') return;
         
+        let nextPlayer = players[0]
+        
+       
+
+
+        console.log(currentPlayer.name)
+
+        if (gameOver) return;
+        if (square.textContent !== '') return;
+
+        console.log(currentPlayer)
         gameBoard.markSquare(square, currentPlayer.token);
         
         switchPlayers();
+           
+        if(currentPlayer === players[0]){
+            nextPlayer = players[1]
+        }
+        turnDisplay.textContent = `${nextPlayer.name}  (${nextPlayer.token}) turn `;
+      
+
+        const updateDisplay = (toWhatHappened = '')=>{
+            
+            roundDisplay.textContent = toWhatHappened
+                      
+        }
 
         const checkWin = () =>{
-            console.log(currentPlayer)
             const isTie = boardRow.every(row => row.every(square => square.textContent !== ''));
-           
+            
             
             for(b = 0; b<3; b++){
                 if (boardRow[b].map(square => square.textContent).join('') === winCondition) {
-                    prompt(`${currentPlayer.name} won`)
+                    updateDisplay(`${currentPlayer.name} won!`)
                     gameOver = true;
                     return;
                 }
                 if (boardColumn[b].map(square => square.textContent).join('') === winCondition) {
-                    prompt(`${currentPlayer.name} won`)
+                    updateDisplay(`${currentPlayer.name} won`)
                     gameOver = true;
                     return;
                 }
                 if (boardDiagonal[b].map(square => square.textContent).join('') === winCondition) {
-                    prompt(`${currentPlayer.name} won`)
+                    updateDisplay(`${currentPlayer.name} won`)
                     gameOver = true;
                     return;
-                }                                   
+                }
+                                     
             }
                
             if (isTie && !gameOver) {
-                prompt("It's a tie!");
+                updateDisplay("It's a tie!");
                 gameOver = true;
+                return;
             }
+           
         }    
         checkWin();
 
+        if(gameOver){
+            const button = document.createElement("button")
+            button.classList.add('restart')
+            button.textContent = "RESTART"
+            roundDisplay.appendChild(button);
+
+            turnDisplay.remove();
+            const restartButton = document.querySelector(".restart")
+            restartButton.addEventListener('click', ()=>{
+                roundDisplay.textContent = '';
+                
+                gameBoard.clearBoard();
+            })
+
+        }
+
                
     }
+    const clearGame = () => {
+        gameOver = false;
+    };
     
     return{
-        playRound
+        playRound,
+        setPlayers,
+        gameOver,
+        players,
+        clearGame
+
     }
 
 })()
 
-gameBoard.squares.forEach(square => {
-    square.addEventListener('click', () => gameController.playRound(square))       
-});
 
+
+gameBoard.squares.forEach((square) => {
+    square.addEventListener("click", () => {gameController.playRound(square); console.log(gameController.gameOver)}
+    );
+});
 
 /*gameBoard.squareMark() */
 
